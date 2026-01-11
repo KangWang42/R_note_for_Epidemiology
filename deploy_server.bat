7a48858691b89525a6754ea2742690624f80286f
KangWang <888666wang4286@gmail.com>
2026-01-11 14:38:10 +0800

    Update server path

--- deploy_server.bat
+++ deploy_server.bat
@@ -7,13 +7,13 @@ echo.
 echo 正在上传文件到服务器...
 echo.
 
-scp -o StrictHostKeyChecking=no -r "E:\99 其它\05 R语言\public\*" root@112.74.48.143:/opt/1panel/apps/openresty/openresty/www/sites/r.wk8686.top/index/
+scp -o StrictHostKeyChecking=no -r "E:\99 其它\05 R语言\public\*" root@112.74.48.143:/opt/1panel/www/sites/r.wk8686.top/index/
 
 echo.
 if %errorlevel% equ 0 (
     echo [成功] 文件上传完成！
     echo 正在修复文件权限...
-    ssh root@112.74.48.143 "chown -R 1000:1000 /opt/1panel/apps/openresty/openresty/www/sites/r.wk8686.top/index/"
+    ssh root@112.74.48.143 "chown -R 1000:1000 /opt/1panel/www/sites/r.wk8686.top/index/"
     echo [成功] 部署完成！
     echo 访问地址: https://r.wk8686.top
 ) else (
